d0618fcc25da9dcc137bb33d1da37a34e187c3aa
Subhash Adhikari <159997388+SubhashAdhikari@users.noreply.github.com>
2025-04-08 23:49:48 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bedtime Story App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system, //
+      home: HomeScreen(), //
+    );
+  }
+}
